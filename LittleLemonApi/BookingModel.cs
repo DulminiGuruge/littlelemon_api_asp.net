b4bcbc25fa11782772ca4b6319c674780d338cdd
dulminiguruge <dkguruge@gmail.com>
2023-12-07 11:34:18 -0600

    Commit

---
+++ LittleLemonApi/BookingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LittleLemonApi
+{
+	public class BookingModel
+	{
+        public string? Fullname { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Occasion { get; set; }
+        public int Guests { get; set; }
+        public string? ReservationTime { get; set; }
+        public string? ReservationDate { get; set; }
+    }
+}
+
---
+++ LittleLemonApi/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LittleLemonApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private static List<BookingModel> bookings = new List<BookingModel>();
+
+        [HttpPost]
+        public IActionResult PostBooking([FromBody] BookingModel booking)
+        {
+            // Perform necessary logic (e.g., store the booking information in a list)
+            bookings.Add(booking);
+            // For simplicity, we'll return the received data as JSON
+            return Ok(booking);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBookings()
+        {
+            // Return all bookings as JSON
+            return Ok(bookings);
+        }
+    }
+}
+
